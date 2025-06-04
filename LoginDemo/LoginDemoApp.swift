c483a454e6b6a9d89f0ea7808ca4933b9dbf4910
airtimeEnthusiast <thewrightguy101@gmail.com>
2025-06-04 08:49:53 -0700

    Initial Commit

---
+++ LoginDemo/LoginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginDemoApp.swift
+//  LoginDemo
+//
+//  Created by Austin Wright on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
