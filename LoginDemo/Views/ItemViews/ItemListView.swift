267565eac71e33e8bcc4fb8d2d21340a702027fb
airtimeEnthusiast <thewrightguy101@gmail.com>
2025-06-04 15:55:44 -0700

    Implementing individual search on the item list.

--- LoginDemo/Views/ItemViews/ItemListView.swift
+++ LoginDemo/Views/ItemViews/ItemListView.swift
@@ -5,22 +5,46 @@
 //  Created by Austin Wright on 6/4/25.
 //
 import SwiftUI
+
+// MARK: List of Items
 struct ItemListView: View {
     @StateObject private var vm: ItemListViewModel = ItemListViewModel()
-    
+    @State private var searchText: String = ""
+
     var body: some View {
         NavigationSplitView {
             List {
-                ForEach(vm.models, id: \.self){ model in
-                    NavigationLink{
-                        ItemDetailView(item: model)
-                    }label:{
-                        Text(model.title)
+            //MARK: Show entire list if the list isn't being searched
+                if let model = vm.selectedModel {
+                    if let item = vm.selectedModel {
+                        NavigationLink{
+                            ItemDetailView(item: item)
+                        }label:{
+                            Text(item.title)
+                        }
+                    }
+                } else{
+                    ForEach(vm.models, id: \.self){ model in
+                        NavigationLink{
+                            ItemDetailView(item: model)
+                        }label:{
+                            Text(model.title)
+                        }
                     }
                 }
-            }.onAppear(){
+            }.onAppear{
                 if vm.models.isEmpty{
                     vm.fetchModels()
+                }
+            }
+            .searchable(text: $searchText)
+            .submitLabel(.search)
+            .onSubmit(of: .search) {
+                performSearch()
+            }
+            .onChange(of: searchText) { newValue in
+                if newValue.isEmpty {
+                    vm.selectedModel = nil
                 }
             }
             .refreshable {
@@ -35,5 +59,16 @@ struct ItemListView: View {
     //MARK: handle loading of data models to the list
     func populateList(){
         vm.fetchModels()
+    }
+    
+    //MARK: Search for an item by ID
+    func performSearch() {
+        print("Searching for: \(searchText)")
+        let trimmed = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
+        if let id = Int(trimmed) {
+            vm.fetchModel(id: id)   // Call API Service 
+        } else {
+            vm.selectedModel = nil
+        }
     }
 }
